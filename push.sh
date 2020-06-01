9d5de7c8ed6f10879ab262371fe25bba30ea8ae5
antelle <dmitryvit@gmail.com>
2020-06-01 17:39:52 +0200

    fixed push script

--- push.sh
+++ push.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
-git tag -d v$1 && git push --delete origin v$1 && git commit -am ci && git tag v$1 -m tag && git push --follow-tags
+set -e
+
+git tag -d v$1 2> /dev/null || true
+git push --delete origin v$1
+git commit -am ci
+git tag v$1 -m tag
+git push --follow-tags
