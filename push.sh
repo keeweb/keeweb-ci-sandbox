acadd4018f08939eff92a30e5878e4eb426b467b
antelle <dmitryvit@gmail.com>
2020-06-01 17:41:32 +0200

    ci

--- push.sh
+++ push.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
-set -e
 
-git tag -d v$1 2> /dev/null || true
-git push --delete origin v$1 2> /dev/null || true
+git tag -d v$1
+git push --delete origin v$1
 git commit -am ci
 git tag v$1 -m tag
 git push --follow-tags
