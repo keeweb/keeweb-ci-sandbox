d653d81b4beda8fc768d99a61e242a7002f4612c
antelle <dmitryvit@gmail.com>
2020-06-01 17:40:56 +0200

    ignoring a non-existing tag

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 set -e
 
 git tag -d v$1 2> /dev/null || true
-git push --delete origin v$1
+git push --delete origin v$1 2> /dev/null || true
 git commit -am ci
 git tag v$1 -m tag
 git push --follow-tags
